d80bed98a5543784b2025babcd12f2bacd74253a
zuazo99 <gzuazo63@gmail.com>
2022-03-02 18:53:32 +0100

    trabajando en la interfaz principal

--- app/src/main/java/com/example/proyecto_das/MainActivity.java
+++ app/src/main/java/com/example/proyecto_das/MainActivity.java
@@ -2,18 +2,13 @@ package com.example.proyecto_das;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Notification;
-import android.graphics.Color;
 import android.os.Bundle;
 
-
-
-public class LoginActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_main);
     }
 }
