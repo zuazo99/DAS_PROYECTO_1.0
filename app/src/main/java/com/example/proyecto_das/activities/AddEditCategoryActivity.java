ac7d27937881c3fdb01011ac34f71305341307b8
zuazo99 <gzuazo63@gmail.com>
2022-03-21 20:29:37 +0100

    Implementado la optencion de imagenes desde la galeria

--- app/src/main/java/com/example/proyecto_das/activities/AddEditCategoryActivity.java
+++ app/src/main/java/com/example/proyecto_das/activities/AddEditCategoryActivity.java
@@ -13,7 +13,9 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,8 +25,10 @@ import android.widget.Toast;
 import com.example.proyecto_das.R;
 import com.example.proyecto_das.models.Categoria;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.net.URI;
@@ -90,56 +94,112 @@ public class AddEditCategoryActivity extends AppCompatActivity {
         btnGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                cargarImagenes();
+                // Intent implicito, nos hace falta ver la version de Android etc...
+                //comprobar version actual de android que estamos runeando
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){ // Mayor que la version 6.0
+                    //Comprobar si ha aceptado, no ha aceptado, o nunca se le ha preguntado
+                    if (hasPermission(Manifest.permission.READ_EXTERNAL_STORAGE)){
+                        // ha aceptado
+                        Intent intent = new Intent(Intent.ACTION_PICK);
+                        intent.setType("image/*");
+                        startActivityForResult(Intent.createChooser(intent, "Seleccione la aplicacion"), PHOTO_SELECTED);
+                    }else{
+                        // o  ha denegado o es la primera vez que se le pregunta
+                        if (!shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)){
+                            //No se le ha preguntado aun
+                            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PHOTO_SELECTED);
+
+                        }else{
+                            // Ha denegado
+                            Toast.makeText(AddEditCategoryActivity.this, "Porfavor, acepta los permisos", Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                            i.addCategory(Intent.CATEGORY_DEFAULT);
+                            i.setData(Uri.parse("package:" + getPackageName()));
+                            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); //
+                            i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                            i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS); // excluir del flujo
+                            startActivity(i);
+
+                        }
+                    }
+                }else{
+                    olderVersion();
+                }
+
             }
         });
 
 
     }
 
-    private void cargarImagenes(){
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("image/*");
-        startActivityForResult(Intent.createChooser(intent, "Seleccione la aplicacion"), PHOTO_SELECTED);
-    }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent imageReturnedIntent) {
-        Uri selectedImageUri = null;
-        Uri selectedImage;
-        String filePath = null;
-        switch (requestCode){
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        switch (requestCode) {
             case PHOTO_SELECTED:
-                if (requestCode == Activity.RESULT_OK){
-                    selectedImage = imageReturnedIntent.getData();
-                    String selectedPath = selectedImage.getPath();
-                    if (requestCode == PHOTO_SELECTED){
-                        if(selectedPath != null){
-                            InputStream imageStream = null;
-                            try{
-                                imageStream = getContentResolver().openInputStream(selectedImage);
-                            }catch (FileNotFoundException e){
-                                e.printStackTrace();
-                            }
-
-                            // Transformamos la URI de la imagen a inputStream y este en un Bitmap
-                            Bitmap bmp = BitmapFactory.decodeStream(imageStream);
-
-                            // Ponemos nuestro bitmap en un ImageView que tenagmos en la vista
-
-                            categoryImage.setImageBitmap(bmp);
-                        }
-                    }
+                if (resultCode == Activity.RESULT_OK) {
+                        Uri path = data.getData();
+                        Toast.makeText(this, path.getPath(), Toast.LENGTH_LONG).show();
+                        categoryImage.setImageURI(path);
+//                        Picasso.get().load(new File(path.getPath())).placeholder(R.drawable.placeholder).into(categoryImage, new Callback() {
+//                            @Override
+//                            public void onSuccess() {
+//
+//                            }
+//
+//                            @Override
+//                            public void onError(Exception e) {
+//                                Toast.makeText(AddEditCategoryActivity.this, "Error!!", Toast.LENGTH_LONG).show();
+//                            }
+//                        });
                 }
-
                 break;
+
             default:
                 super.onActivityResult(requestCode, resultCode, data);
 
         }
-
-
     }
+
+
+    //    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent imageReturnedIntent) {
+//        Uri selectedImageUri = null;
+//        Uri selectedImage;
+//        String filePath = null;
+//        switch (requestCode){
+//            case PHOTO_SELECTED:
+//                if (requestCode == Activity.RESULT_OK){
+//                    selectedImage = imageReturnedIntent.getData();
+//                    String selectedPath = selectedImage.getPath();
+//                    if (requestCode == PHOTO_SELECTED){
+//                        if(selectedPath != null){
+//                            InputStream imageStream = null;
+//                            try{
+//                                imageStream = getContentResolver().openInputStream(selectedImage);
+//                            }catch (FileNotFoundException e){
+//                                e.printStackTrace();
+//                            }
+//
+//                            // Transformamos la URI de la imagen a inputStream y este en un Bitmap
+//                            Bitmap bmp = BitmapFactory.decodeStream(imageStream);
+//
+//                            // Ponemos nuestro bitmap en un ImageView que tenagmos en la vista
+//
+//                            categoryImage.setImageBitmap(bmp);
+//                        }
+//                    }
+//                }
+//
+//                break;
+//            default:
+//                super.onActivityResult(requestCode, resultCode, data);
+//
+//        }
+//
+//
+//    }
 
     private void viewReferences(){
         //Metodo para hacer referencia a las views
@@ -216,6 +276,18 @@ public class AddEditCategoryActivity extends AppCompatActivity {
 
     // ** COMPROBAR PERMISOS ** //
 
+
+    private void olderVersion() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        if (hasPermission(Manifest.permission.READ_EXTERNAL_STORAGE)){
+            intent.setType("image/*");
+            startActivityForResult(Intent.createChooser(intent, "Seleccione la aplicacion"), PHOTO_SELECTED);
+        }else{
+            Toast.makeText(this, "No tienes los permisos", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
     private void checkForPermission(){
         int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
 
@@ -233,5 +305,20 @@ public class AddEditCategoryActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PHOTO_SELECTED:
+                if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                    intent.setType("image/*");
+                    startActivityForResult(Intent.createChooser(intent, "Seleccione la aplicacion"), PHOTO_SELECTED);
+                }else{
+                    // No concedido el permiso
+                    Toast.makeText(this, "No tienes los permisos", Toast.LENGTH_SHORT).show();
+
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
