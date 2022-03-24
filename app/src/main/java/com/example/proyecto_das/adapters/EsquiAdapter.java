63e749db4f5ba099e92b1df6526aa6e1c9033b8b
zuazo99 <gzuazo63@gmail.com>
2022-03-24 11:51:09 +0100

    EsquiAdapter acabado

--- app/src/main/java/com/example/proyecto_das/adapters/EsquiAdapter.java
+++ app/src/main/java/com/example/proyecto_das/adapters/EsquiAdapter.java
@@ -1,6 +1,142 @@
 package com.example.proyecto_das.adapters;
 
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class EsquiAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder>{
+import com.example.proyecto_das.R;
+import com.example.proyecto_das.models.Categoria;
+import com.example.proyecto_das.models.Esqui;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.util.List;
+
+public class EsquiAdapter extends RecyclerView.Adapter<EsquiAdapter.ViewHolder>{
+
+    private Context context;
+    private List<Esqui> modelos;
+    private int layout;
+    private OnItemClickListener itemClickListener;
+    private OnButtonClickListener buttonClickListener;
+
+
+    public EsquiAdapter(List<Esqui> modelos, int layout, OnItemClickListener itemClickListener, OnButtonClickListener buttonClickListener) {
+        this.modelos = modelos;
+        this.layout = layout;
+        this.itemClickListener = itemClickListener;
+        this.buttonClickListener = buttonClickListener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(layout, parent, false);
+        context = parent.getContext();
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bind(modelos.get(position), itemClickListener, buttonClickListener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return modelos.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ImageView esquiImagen;
+        public TextView esquiNombreMarca;
+        public TextView esquiNombreModelo;
+        public TextView esquiPrecio;
+        public Button btnDelete;
+        public Button btnEdit;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            esquiImagen = itemView.findViewById(R.id.imageViewEsqui);
+            esquiNombreMarca = itemView.findViewById(R.id.textViewEsquiMarca);
+            esquiNombreModelo = itemView.findViewById(R.id.textViewEsquiNombreModelo);
+            esquiPrecio = itemView.findViewById(R.id.textViewPrecioEsqui);
+            btnDelete = itemView.findViewById(R.id.buttonDeleteEsqui);
+            btnEdit = itemView.findViewById(R.id.buttonEditEsqui);
+        }
+
+        public void bind(final Esqui esqui, final OnItemClickListener itemListener, final OnButtonClickListener btnListener) {
+            esquiNombreMarca.setText(esqui.getNombreMarca());
+            esquiNombreModelo.setText(esqui.getNombreProd());
+            esquiPrecio.setText((int) esqui.getPrecio());
+            // Picasso.get().load(categoria.getImagen()).placeholder(R.drawable.placeholder).fit().into(categoriaFoto); //Libreria usada para el manejo de imagenes
+
+            if (esqui.getFromGalery()){
+                Picasso.get().load(new File(esqui.getImagen())).placeholder(R.drawable.placeholder).fit().into(esquiImagen, new Callback() {
+                    @Override
+                    public void onSuccess() {
+
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        Toast.makeText(context, "Error!!! "+ esqui.getImagen(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+            }else{
+                Picasso.get().load(esqui.getImagen()).placeholder(R.drawable.placeholder).fit().into(esquiImagen);
+
+            }
+
+
+            btnDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    btnListener.onButtonDeleteClick(esqui, getAdapterPosition());
+                }
+            });
+
+            btnEdit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    btnListener.onButtonEditClick(esqui, getAdapterPosition());
+                }
+            });
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    itemListener.onItemClick(esqui, getAdapterPosition());
+                }
+            });
+
+        }
+
+
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(Esqui esqui, int position);
+    }
+
+    public interface OnButtonClickListener{
+        void onButtonDeleteClick(Esqui esqu, int position);
+        void onButtonEditClick(Esqui esqu, int position);
+    }
+
+
+
 }
--- app/src/main/java/com/example/proyecto_das/models/Esqui.java
+++ app/src/main/java/com/example/proyecto_das/models/Esqui.java
@@ -16,21 +16,29 @@ public class Esqui extends RealmObject {
     @Required
     private String nombreProd;
     private String descripcion;
+    private String imagen;
+    private boolean fromGalery;
+
 
     public Esqui(){
 
     }
 
-    public Esqui(String nombreMarca, String nombreProd, String descripcion, double precio) {
+    public Esqui(String nombreMarca, String imagen, String nombreProd, String descripcion, double precio) {
         this.id = MyApplication.EsquiID.incrementAndGet();
         this.precio = precio;
         this.nombreMarca = nombreMarca;
+        this.imagen = imagen;
         this.nombreProd = nombreProd;
         this.descripcion = descripcion;
     }
 
     public int getId() {
         return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
     }
 
     public double getPrecio() {
@@ -63,5 +71,21 @@ public class Esqui extends RealmObject {
 
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public boolean getFromGalery() {
+        return fromGalery;
+    }
+
+    public void setFromGalery(boolean fromGalery) {
+        this.fromGalery = fromGalery;
     }
 }
